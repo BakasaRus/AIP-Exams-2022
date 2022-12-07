46814467dd450c14cb8d84c1d63184730a951b76
Nikolay Andreev <bakasaru@list.ru>
2022-12-07 15:10:46 +0300

    fix: increase range in A1.19

--- A1.19.py
+++ A1.19.py
@@ -1,5 +1,5 @@
-for A in range(1, 1000):
-    for x in range(1, 1000):
+for A in range(1, 10000):
+    for x in range(1, 50000):
         f = (x % A == 0) <= ((x % 15 == 0) or (x % 23 != 0)) and (x + A >= 150)
         if not f:
             break
